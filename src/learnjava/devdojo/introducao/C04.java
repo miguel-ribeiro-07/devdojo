215ae954e35cdf5489b5d300bdfa637b95b1e688
Miguel Ribeiro <miguel.ribeiro.07@protonmail.com>
2024-08-09 00:33:28 -0300

    Operators

--- src/learnjava/devdojo/introducao/C04.java
+++ src/learnjava/devdojo/introducao/C04.java
@@ -8,9 +8,27 @@ public class C04 {
         int numb01 = 2;
         double numb02 = 5;
         double result = numb01 /numb02;
-        System.out.println("Value: " + (numb01 + numb02));
-        System.out.println(result);
+        //System.out.println("Value: " + (numb01 + numb02));
+        //System.out.println(result);
 
         // % operator
+
+        int module = 35 % 7;
+        //System.out.println(module);
+
+        // relational operators
+
+        int x = 2;
+        boolean isTwiceXBiggerThen = (x*2) > 10;
+        //System.out.println(isTwiceXBiggerThen);
+
+        //logical operators in pratice
+
+        int age = 23;
+        float salary = 2000F;
+        boolean isOnTheImigrantRulesAtThirty;
+        isOnTheImigrantRulesAtThirty = (age < 30 && salary >= 3381) || (age >= 30 && salary >= 4612);
+
+        System.out.println("isOnTheImigrantRulesAtThirty: " + isOnTheImigrantRulesAtThirty);
     }
 }

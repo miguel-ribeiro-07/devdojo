aa831d458974cedc7592a09f51eaa9c90d591175
Miguel Ribeiro <miguel.ribeiro.07@protonmail.com>
2024-08-11 00:18:41 -0300

    Test push

--- src/learnjava/devdojo/introducao/C04.java
+++ src/learnjava/devdojo/introducao/C04.java
@@ -23,12 +23,43 @@ public class C04 {
         //System.out.println(isTwiceXBiggerThen);
 
         //logical operators in pratice
-
+        /**
+         * AND
+         */
         int age = 23;
         float salary = 2000F;
         boolean isOnTheImigrantRulesAtThirty;
         isOnTheImigrantRulesAtThirty = (age < 30 && salary >= 3381) || (age >= 30 && salary >= 4612);
 
         System.out.println("isOnTheImigrantRulesAtThirty: " + isOnTheImigrantRulesAtThirty);
+
+        /**
+         * OR
+         */
+
+        float myCurrentBudget = 250.10F;
+        float myVaultMoney = 4000.50F;
+        float playstationCost = 3999.99F;
+        boolean isPlaystationPurchasable;
+
+        isPlaystationPurchasable = (myCurrentBudget >= playstationCost) || (myCurrentBudget + myVaultMoney >= playstationCost);
+        //System.out.println("Current budget: "+ (myCurrentBudget + myVaultMoney) +  " | Playstation price: "+ playstationCost + " isPlaystationPurchasable: " +isPlaystationPurchasable);
+
+        // ASSIGNMENT Operators
+        double exampleNumber = 1000.5D;
+        exampleNumber += 200;
+        exampleNumber -= 200;
+        exampleNumber *= 2;
+        exampleNumber /= 2;
+        exampleNumber %= 1;
+
+        System.out.println(exampleNumber);
+
+        //Unary - increment
+        int count = 0;
+        count ++;
+        count --;
+        System.out.println(count++);
+        System.out.println(count);
     }
 }

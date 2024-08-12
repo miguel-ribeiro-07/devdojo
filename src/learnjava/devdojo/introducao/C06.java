8697fcb77f4339385c3e7aee854307f37fd1fcbf
Miguel Ribeiro <miguel.ribeiro.07@protonmail.com>
2024-08-12 00:37:57 -0300

    Local variables concepts devdojo

--- src/learnjava/devdojo/introducao/C06.java
+++ src/learnjava/devdojo/introducao/C06.java
@@ -2,6 +2,20 @@ package learnjava.devdojo.introducao;
 
 public class C06 {
     public static void main(String[] args){
+        // age < 15 category junior
+        // age >=15 && age < 18 category senior
+        // age >=18 category masters
 
+        int age = 10;
+        String category;
+
+        if(age < 15){
+            category = "Junior category";
+        } else if (age >= 15 && age < 18) {
+            category = "Senior category";
+        } else{
+            System.out.println("Isn´t inside");
+        }
+        System.out.println(category);
     }
 }

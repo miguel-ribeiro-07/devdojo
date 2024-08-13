ab94dcce5b54215c495c0075e0a4acd68f4509e3
Miguel Ribeiro <miguel.ribeiro.07@protonmail.com>
2024-08-13 00:17:37 -0300

    taxes netherlands

--- src/learnjava/devdojo/introducao/C07.java
+++ src/learnjava/devdojo/introducao/C07.java
@@ -1,12 +1,22 @@
 package learnjava.devdojo.introducao;
 
 public class C07 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //ternary operators
-        int salary = 500;
-        float ps5Cost = 4000;
+        float salary = 500F;
+        float ps5Cost = 4000F;
         String canBuyPs5 = (12 * salary) >= ps5Cost ? "i can but it in one year" : "i can´t buy this ps5 =(";
 
         System.out.println(canBuyPs5);
+
+        // TAXES in Netherlands
+        float salaryPerMonth = 5000.0F;
+        float tax;
+        if(salaryPerMonth <= 34712.00F){
+            tax = 9.7F;
+        }else if(salaryPerMonth <= 68507.00F){
+            tax = 37.35F;
+        }else tax = 49.5F;
+        System.out.print(tax + " %");
     }
 }

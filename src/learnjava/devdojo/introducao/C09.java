6e75eb2f67249b0ef0e3521f61ce8821e83b4711
Miguel Ribeiro <miguel.ribeiro.07@protonmail.com>
2024-08-17 03:19:40 -0300

    Switch

--- src/learnjava/devdojo/introducao/C09.java
+++ src/learnjava/devdojo/introducao/C09.java
@@ -1,4 +1,34 @@
 package learnjava.devdojo.introducao;
 
 public class C09 {
+
+    public static void main(String[] args) {
+        byte dayInNumber = 10;
+
+        switch(dayInNumber){
+            case 1:
+                System.out.println("Day : "+ dayInNumber + ", equal Sunday, so Weekend day");
+                break;
+            case 2:
+                System.out.println("Day " + dayInNumber + ", equal Monday, so Business day");
+                break;
+            case 3:
+                System.out.println("Day " + dayInNumber + ", equal Tuesday, so Business day");
+                break;
+            case 4:
+                System.out.println("Day " + dayInNumber + ", equal Wednesday, so Business day");
+                break;
+            case 5:
+                System.out.println("Day " + dayInNumber + ", equal Thursday, so Business day");
+                break;
+            case 6:
+                System.out.println("Day " + dayInNumber + ", equal Friday, so Business day");
+                break;
+            case 7:
+                System.out.println("Day " + dayInNumber + ", equal Saturday, so Weekend day");
+                break;
+            default:
+                System.out.println("Out of range");
+        }
+    }
 }

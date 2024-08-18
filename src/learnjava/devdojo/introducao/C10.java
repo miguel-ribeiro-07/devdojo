12814379a480190a39ca3ae396d40f88fa05ddcd
Miguel Ribeiro <miguel.ribeiro.07@protonmail.com>
2024-08-18 02:55:04 -0300

    loop

--- src/learnjava/devdojo/introducao/C10.java
+++ src/learnjava/devdojo/introducao/C10.java
@@ -4,9 +4,18 @@ public class C10 {
     public static void main(String[] args) {
         byte num = 1;
 
-        while(num <= 10){
+        /*while(num <= 10){
             System.out.println("Counting " + num);
             num++;
+        }*/
+
+        /*do{
+            System.out.println("Counting version 2.0 " + num);
+            num++;
+        }while(num <= 10);*/
+
+        for(int i = 0; i <= 10; i++){
+            System.out.println("Counting " + i);
         }
     }
 }

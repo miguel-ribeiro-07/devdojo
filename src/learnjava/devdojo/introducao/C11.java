62411a0cff9f748db70bf16b19935bf90e434dc6
Miguel Ribeiro <miguel.ribeiro.07@protonmail.com>
2024-08-20 01:51:25 -0300

    Seventeen

---
+++ src/learnjava/devdojo/introducao/C11.java
@@ -0,0 +1,26 @@
+package learnjava.devdojo.introducao;
+
+import java.util.Arrays;
+
+public class C11 {
+    public static void main(String[] args){
+        /*String[] cars = {"car 1", "car 2"};
+        System.out.println(Arrays.toString(cars));*/
+
+        int pointer = 0;
+
+        /*for(int i = 0; i <= 1000; i++){
+          if(i % 2 == 0) System.out.println(i);
+        }*/
+
+        /*while(pointer <= 10){
+            if(pointer % 2 == 0) System.out.println(pointer);
+            pointer++;
+        }*/
+
+        do{
+            if(pointer % 2 == 0) System.out.println(pointer);
+            pointer++;
+        }while(pointer <= 10);
+    }
+}

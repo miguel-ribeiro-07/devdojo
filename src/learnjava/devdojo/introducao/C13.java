b616de70ef49a4395c3dd858728696287a764976
Miguel Ribeiro <miguel.ribeiro.07@protonmail.com>
2024-08-22 02:36:28 -0300

    Exercise car installments

--- src/learnjava/devdojo/introducao/C13.java
+++ src/learnjava/devdojo/introducao/C13.java
@@ -1,4 +1,18 @@
 package learnjava.devdojo.introducao;
 
 public class C13 {
+    public static void main(String[] args){
+        float valueCar = 13500;
+        int installments = 0;
+        float installmentsValue = valueCar;
+
+        while(installmentsValue > 1000){
+            if(installments <= 0) installments = 1;
+            installmentsValue = valueCar / installments;
+            if(installmentsValue < 1000) break;
+            System.out.println("Value: " + installmentsValue + " Installments/Months: " + installments);
+            installments++;
+        }
+
+    }
 }

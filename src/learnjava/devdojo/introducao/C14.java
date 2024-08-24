e21781fc41d7a1e789f3bd3ba094594d9aee6678
Miguel Ribeiro <miguel.ribeiro.07@protonmail.com>
2024-08-24 01:58:53 -0300

    Assign

--- src/learnjava/devdojo/introducao/C14.java
+++ src/learnjava/devdojo/introducao/C14.java
@@ -3,6 +3,7 @@ package learnjava.devdojo.introducao;
 public class C14 {
     public static void main(String[] args) {
         int[] ages = new int[2];
+        ages[1] = 10;
 
         System.out.println(ages[1]);
         System.out.println(ages[0]);

03b6b74d11cb5adb0cc10a9c86583295e923057f
Miguel Ribeiro <miguel.ribeiro.07@protonmail.com>
2024-08-24 01:57:03 -0300

    Arrays concepts in java

---
+++ src/learnjava/devdojo/introducao/C14.java
@@ -0,0 +1,10 @@
+package learnjava.devdojo.introducao;
+
+public class C14 {
+    public static void main(String[] args) {
+        int[] ages = new int[2];
+
+        System.out.println(ages[1]);
+        System.out.println(ages[0]);
+    }
+}

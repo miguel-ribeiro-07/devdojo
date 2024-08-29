2b5cf5bd17ee68d8c50afa71972b0d86cbdeb8a7
Miguel Ribeiro <miguel.ribeiro.07@protonmail.com>
2024-08-29 01:32:03 -0300

    Exercise

--- src/learnjava/devdojo/introducao/C14.java
+++ src/learnjava/devdojo/introducao/C14.java
@@ -3,9 +3,15 @@ package learnjava.devdojo.introducao;
 public class C14 {
     public static void main(String[] args) {
         int[] ages = new int[2];
+        double[] doubNumbs = new double[4];
         ages[1] = 10;
+        doubNumbs[2] = 3.4;
+
+        String[] test2 = new String[2];
+
+        System.out.println(test2[1]);
 
         System.out.println(ages[1]);
-        System.out.println(ages[0]);
+        System.out.println(doubNumbs[2]);
     }
 }
